bbec8daed7ebc77fbd990bf7b52a389058253424
wangy325 <wangy325@qq.com>
2024-07-11 23:42:05 +0800

    c: fix code and code comment update

--- c/myval.c
+++ c/myval.c
@@ -6,9 +6,7 @@ int myval;
 int main(int argc, char *argv[])
 {
     myval = atoi(argv[1]);
-    int count = 10;
-    while(count > 0){
+    while(1){
         printf("myval is %d, loc 0x%lx\n", myval, (long)&myval);
-        count--;
     }
 }
