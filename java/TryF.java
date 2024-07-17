47a4a76c15f902b23ff0f1ceb829265553713974
wangy325 <wangy325@qq.com>
2024-07-18 01:49:20 +0800

    py: execption

---
+++ java/TryF.java
@@ -0,0 +1,39 @@
+/**
+ * Author: wangy325
+ * Date: 2024-07-17 12:37:27
+ * Description: 不要在finally块里写返回语句
+ **/
+
+class TryF {
+    public static void main(String[] args) {
+        System.out.println(how_finally_works(2, 3));
+        System.out.println("---------");
+        System.out.println(how_finally_works(2, 0));
+        System.out.println("---------");
+        System.out.println(how_finally_works("a", "b"));
+        System.out.println("---------");
+
+    }
+
+    /**
+     * 和python的表现一模一样
+     * 
+     * @param x
+     * @param y
+     * @return
+     * @see try_excep.py
+     */
+    @SuppressWarnings("finally")
+    static float how_finally_works(Object x, Object y) {
+        try {
+            return (int) x / (int) y;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            System.out.println("can not devide by 0");
+            return 0;
+        } finally {
+            System.out.println("finally...");
+            return -1;
+        }
+    }
+}
