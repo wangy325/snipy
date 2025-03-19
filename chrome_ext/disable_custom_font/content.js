b23eb05079b005a2728563d8253d3f689bcafbda
wangy325-win <wangy325@qq.com>
2025-03-19 23:39:25 +0800

    fix: chrome font ext adds support for taobao, ssp. And setting default font to `NotoSans`.

--- chrome_ext/disable_custom_font/content.js
+++ chrome_ext/disable_custom_font/content.js
@@ -4,7 +4,7 @@ const isIconFontPage = () => {
   return styles.some(styleSheet => {
     try {
       const rules = Array.from(styleSheet.cssRules || []);
-      return rules.some(rule => rule.cssText.includes('font-family') && rule.cssText.includes('FontAwesome'));
+      return rules.some(rule => rule.cssText.includes('font-family'));
     } catch (e) {
       return false; // 处理跨域样式表的异常
     }
@@ -16,22 +16,32 @@ if (!isIconFontPage()) {
   // 没有默认字体的网页，强制所有元素使用浏览器默认字体
   style.textContent = `
       * {
-          font-family: inherit !important; /* 使用浏览器默认字体 */
+          font-family:  "NotoSans", "inherit" !important; /* 使用浏览器默认字体 */
           font-display: swap
       }
   `;
 } else {
   style.textContent = `
-    body, p, div, span, h1, h2, h3, h4, h5, h6 {
-        font-family: inherit !important; /* 使用浏览器默认字体 */
+    body, p, li, div, span, h1, h2, h3, h4, h5, h6 {
+        font-family: "NotoSans", "inherit"  !important; 
     }
-    /* 保留网页的图标字体 */
-    @font-face {
-        font-family: 'FontAwesome'; /* 将 'CustomFont' 替换为你想保留的图标字体名称 */
-        font-family: 'FontAwesome', 'Glyphicons Halflings', 'Material Icons', 'sans-serif' !important;
-        src: local('FontAwesome');  /* 替换为实际的图标字体文件名 */
-        font-display: swap;
-    }
+
+    *[class^="fa"],
+    *[class*=" fa"],  /* Font Awesome */
+    *[class^="glyphicon"],
+    *[class*=" glyphicon"], /* Glyphicon */
+    *[class^="material-icons"],
+    *[class*=" material-icons"], /* Material Icons */
+    *[class*="icon"],   /* taobao, ssp*/
+    .icon, .material-symbols-outlined { 
+      font-family: 'FontAwesome',
+         'Glyphicons Halflings',
+         'Material Icons',
+         'sans-serif',
+         'global-iconfont', /* taobao.com*/
+         'sspai_community_icon' /* ssp */
+         !important; 
+   }
   `;
 }
 // 将样式元素添加到文档头部
