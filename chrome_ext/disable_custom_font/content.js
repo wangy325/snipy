a323a928016ef86f5032bbae0bdcbfc7bedbc2b6
wangy325-win <wangy325@qq.com>
2025-03-20 21:37:12 +0800

    fix: chrome-ext adds icon-font support for some extra pages. Some Traditional Chinese pages font are too thin, and can not easily modified by setting font-weight

--- chrome_ext/disable_custom_font/content.js
+++ chrome_ext/disable_custom_font/content.js
@@ -16,15 +16,27 @@ if (!isIconFontPage()) {
   // 没有默认字体的网页，强制所有元素使用浏览器默认字体
   style.textContent = `
       * {
-          font-family:  "NotoSans", "inherit" !important; /* 使用浏览器默认字体 */
+          font-family:  "SourceHanSans SC VF","SourceHanSans HC", "NotoSans SC", "Arial", "Roboto", "inherit" !important; /* 使用浏览器默认字体 */
           font-display: swap
       }
+
+      *[lang^="zh-Hant"], *[lang^="zh-TW"] { 
+        font-weight: 600; 
+     }
   `;
 } else {
   style.textContent = `
     body, p, li, div, span, h1, h2, h3, h4, h5, h6 {
-        font-family: "NotoSans", "inherit"  !important; 
+        font-family: "SourceHanSans SC VF","SourceHanSans HC", "NotoSans SC", "Arial", "Roboto", "inherit" !important; 
     }
+
+    /* doesn't work for all pages*/
+    div[lang^="zh-Hant"],
+    div[lang^="zh-TW"],
+    p[lang^="zh-Hant"],
+    p[lang^="zh-TW"]{ 
+        font-weight: 600 !important; 
+     }
 
     *[class^="fa"],
     *[class*=" fa"],  /* Font Awesome */
@@ -33,13 +45,16 @@ if (!isIconFontPage()) {
     *[class^="material-icons"],
     *[class*=" material-icons"], /* Material Icons */
     *[class*="icon"],   /* taobao, ssp*/
+     [class*=" icon-"], [class^=icon-] /* smzdm*/
     .icon, .material-symbols-outlined { 
       font-family: 'FontAwesome',
-         'Glyphicons Halflings',
+         'Glyphicons Halflings',  /* python anywhere*/
          'Material Icons',
          'sans-serif',
          'global-iconfont', /* taobao.com*/
-         'sspai_community_icon' /* ssp */
+         'sspai_community_icon', /* ssp */
+         'zdm-icons', /* smzdm */
+         'iconfont' /* jd */ 
          !important; 
    }
   `;
